22d7b625cc2049c29650334cec68d933858f9307
Phan Vu <anhvu2182004@gmail.com>
2025-03-15 12:35:15 +0700

    [ADD]: Add new migration

---
+++ Repositories/Migrations/20250315053059_Add CancelReason + RejectReason + IsMentorCanceled to Appointment.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCancelReasonRejectReasonIsMentorCanceledtoAppointment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CancelReason",
+                table: "Appointment",
+                type: "varchar(2000)",
+                maxLength: 2000,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMentorCanceled",
+                table: "Appointment",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RejectReason",
+                table: "Appointment",
+                type: "varchar(2000)",
+                maxLength: 2000,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CancelReason",
+                table: "Appointment");
+
+            migrationBuilder.DropColumn(
+                name: "IsMentorCanceled",
+                table: "Appointment");
+
+            migrationBuilder.DropColumn(
+                name: "RejectReason",
+                table: "Appointment");
+        }
+    }
+}
