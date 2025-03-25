b382676da941719bd1d218407781fb2c8139dff8
khanght <khanghuynhtan.work@gmail.com>
2025-03-25 13:29:47 +0700

    [REMOVE]: Remove authorize get mentor's availability

--- FPTMentorLink-Backend/Controllers/MentorController.cs
+++ FPTMentorLink-Backend/Controllers/MentorController.cs
@@ -22,34 +22,5 @@ public class MentorController : ControllerBase
         var result = await _mentorService.GetMentorsPagedAsync(request);
         return result.IsSuccess ? Ok(result.Value) : BadRequest(result);
     }
-
-    [HttpGet("my-appointments")]
-    [Authorize(Roles = "Mentor")]
-    public async Task<IActionResult> GetMyAppointments([FromQuery] GetMentorAppointmentsRequest request)
-    {
-        var userId = User.GetUserId();
-        if (userId.IsNullOrGuidEmpty())
-        {
-            return BadRequest(Result.Failure("User not found"));
-        }
-
-        request.MentorId = userId!.Value;
-        var result = await _mentorService.GetMyAppointmentPagedAsync(request);
-        return result.IsSuccess ? Ok(result.Value) : BadRequest(result);
-    }
-
-    [HttpGet("my-transactions")]
-    [Authorize(Roles = "Mentor")]
-    public async Task<IActionResult> GetMyTransactions([FromQuery] GetMentorTransactionsRequest request)
-    {
-        var userId = User.GetUserId();
-        if (userId.IsNullOrGuidEmpty())
-        {
-            return BadRequest(Result.Failure("User not found"));
-        }
-
-        request.MentorId = userId!.Value;
-        var result = await _mentorService.GetMyTransactionPagedAsync(request);
-        return result.IsSuccess ? Ok(result.Value) : BadRequest(result);
-    }
+    
 }
